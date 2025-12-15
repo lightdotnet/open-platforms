b3ce50dfe3326afeff874c7ea246d168d538f498
minhvu <84697874+mivud@users.noreply.github.com>
2025-12-15 19:13:32 +0700

    update GrabExpress

---
+++ src/GrabSdk.GrabExpress.Core/Common/Courier.cs
@@ -0,0 +1,20 @@
+﻿namespace Light.Grab.GrabExpress.Common
+{
+    public class Courier
+    {
+        public string name { get; set; }
+        public string phone { get; set; }
+        public string pictureURL { get; set; }
+        public int rating { get; set; }
+        public Coordinates coordinates { get; set; }
+        public Vehicle vehicle { get; set; }
+        public object extraInfo { get; set; }
+    }
+
+    public class Vehicle
+    {
+        public string licensePlate { get; set; }
+        public string model { get; set; }
+        public string vehicleType { get; set; }
+    }
+}
